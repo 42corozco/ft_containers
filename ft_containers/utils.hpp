51e95b6b5c4b60a6c3c7199b72f55ab613d5f95b
42corozco <42corozco@gmail.com>
2022-06-08 03:50:42 +0200

    add pair

--- ft_containers/utils.hpp
+++ ft_containers/utils.hpp
@@ -12,6 +12,7 @@ namespace ft
 
 	template<class T>
 	struct enable_if<true, T>	{ typedef T type; };
+	/**********[fin-enable_if]**********/
 
 	/**********[equal]**********/
 	//	->	https://www.cplusplus.com/reference/algorithm/equal/
@@ -42,6 +43,7 @@ namespace ft
 		}
 		return true;
 	};
+	/**********[fin-equal]**********/
 
 	/**********[lexicographical_compare]**********/
 	//	->	https://en.cppreference.com/w/cpp/algorithm/lexicographical_compare
@@ -76,7 +78,67 @@ namespace ft
 		}
 		return ((first1 == last1) && (first2 != last2));
 	};
+	/**********[finlexicographical_compare]**********/
 
+	template <class Input, class Output>
+	struct selectFirst : public std::unary_function<Input, Output>
+	{
+		const Output &operator()(const Input &x) const
+		{
+			return (x.first);
+		}
+	};
+
+	/**********[PAIR]**********/
+	// -> https://en.cppreference.com/w/cpp/utility/pair
+
+    template<class T1, class T2>
+	struct pair
+	{
+		//Member objects
+		typedef T1	first_type;
+		typedef T2	second_type;
+
+		//member types
+		first_type	first;
+		second_type	second;
+
+		//members functions
+		pair() : first(first_type()), second(second_type()) {};
+		template <class U, class V>
+		pair(const pair<U,V> &pr) : first(pr.first), second(pr.second) {};
+		pair(const first_type &a, const second_type &b) : first(a), second(b) {};
+
+		pair	&operator=(const pair &pr)
+		{
+			first = pr.first;
+			second = pr.second;
+			return (*this);
+		}
+	};
+
+	//(Non-member functions)
+	template <class T1, class T2>
+	ft::pair<T1, T2>	make_pair(T1 x, T2 y) { return (ft::pair<T1, T2>(x, y)); }
+
+	template <class T1, class T2>
+	bool	operator==(const pair<T1,T2>& lhs, const pair<T1,T2>& rhs) { return (lhs.first == rhs.first && lhs.second == rhs.second); }
+
+	template <class T1, class T2>
+	bool	operator!=(const pair<T1,T2>& lhs, const pair<T1,T2>& rhs) { return (!(lhs == rhs)); }
+
+	template <class T1, class T2>
+	bool	operator<(const pair<T1,T2>& lhs, const pair<T1,T2>& rhs) { return (lhs.first < rhs.first || (!(rhs.first < lhs.first) && lhs.second < rhs.second)); }
+
+	template <class T1, class T2>
+	bool	operator<=(const pair<T1,T2>& lhs, const pair<T1,T2>& rhs) { return (!(rhs < lhs)); }
+
+	template <class T1, class T2>
+	bool	operator>(const pair<T1,T2>& lhs, const pair<T1,T2>& rhs) { return (rhs < lhs); }
+
+	template <class T1, class T2>
+	bool	operator>=(const pair<T1,T2>& lhs, const pair<T1,T2>& rhs) { return (!(lhs < rhs)); }
+	/**********[FIN-PAIR]**********/
 };
 
 #endif
